4aa888292380497755dab36a05ecb2f542ad7006
ClarkMitchell <clarkmitchelldev@gmail.com>
2023-01-16 13:37:42 -0500

    init

--- app/docs/page.jsx
+++ app/docs/page.jsx
@@ -1,14 +1,11 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import styles from '../page.module.css';
 
-export default function Home() {
+export default function Docs() {
   return (
     <>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.jsx</code>
-        </p>
+        <p>Docs</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,9 @@
-import './globals.css'
+import './globals.css';
+import { Inter } from '@next/font/google';
+import Link from 'next/link';
+import styles from './page.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +13,41 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <main className={styles.main}>
+          {children}
+
+          <div className={styles.grid}>
+            <Link href="/docs" className={styles.card}>
+              <h2 className={inter.className}>
+                Docs <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Find in-depth information about Next.js features and API.
+              </p>
+            </Link>
+
+            <Link href="/templates" className={styles.card}>
+              <h2 className={inter.className}>
+                Templates <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Explore the Next.js 13 playground.
+              </p>
+            </Link>
+
+            <Link href="/deploy" className={styles.card}>
+              <h2 className={inter.className}>
+                Deploy <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Instantly deploy your Next.js site to a shareable URL with
+                Vercel.
+              </p>
+            </Link>
+          </div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
